4392964844ffe0196c781449e0d40cec912f6241
Karishma Hossain <karishmah@gmail.com>
2019-04-23 20:39:27 -0700

    add id names

---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+// Create variable employeeName 
+
+var employeeName = "";
+
+// Store user input 
+
+
+// Create variable Role
+var role = "";
+// Store user input 
+
+// Create variable startDate
+var startDate = "";
+
+// Store user input
+
+// Create monthly Rate
+var monthlyRate = "";
+
+// store user input
+
+// on click even for the submit button
+
+var employeeName = $("#employeeName").val();
+var role = $("#role").val();
+var startDate = $("#startDate").val();
+var monthlyRate = $("#monthlyRate").val();
+
+// create function for months worked (current date - start date)
+
+// create function for total billed (months * monthly rate)
+
+// then function respond
+
+// create table row
