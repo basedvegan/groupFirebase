3c0812924f5e3392ae5511994a0153c2fa1cdd97
Rusty Boot <nicholastuso@yahoo.com>
2019-04-23 19:33:49 -0700

    refresh

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCHlCWtnFNCBK1zdm0qSSKI9SYZfuXd8ao",
+    authDomain: "groupfirebase-d46b1.firebaseapp.com",
+    databaseURL: "https://groupfirebase-d46b1.firebaseio.com",
+    projectId: "groupfirebase-d46b1",
+    storageBucket: "groupfirebase-d46b1.appspot.com",
+    messagingSenderId: "3585038109"
+  };
+firebase.initializeApp(config);
+var db=firebase.database();
